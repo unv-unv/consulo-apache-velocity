69626ce4e210e3754c52ca1fcbec9824e9c99d1b
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-04-17 16:38:46 +0300

    drop ide api deps

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,34 @@
 module com.intellij.velocity
 {
     requires consulo.ide.api;
+
+    requires consulo.application.api;
+    requires consulo.application.content.api;
+    requires consulo.code.editor.api;
+    requires consulo.color.scheme.api;
+    requires consulo.component.api;
+    requires consulo.document.api;
+    requires consulo.file.editor.api;
+    requires consulo.file.template.api;
+    requires consulo.index.io;
+    requires consulo.language.api;
+    requires consulo.language.impl;
+    requires consulo.language.editor.api;
+    requires consulo.language.editor.refactoring.api;
+    requires consulo.language.code.style.api;
+    requires consulo.module.api;
+    requires consulo.module.content.api;
+    requires consulo.navigation.api;
+    requires consulo.project.api;
+    requires consulo.ui.api;
+    requires consulo.ui.ex.api;
+    requires consulo.usage.api;
+    requires consulo.virtual.file.system.api;
+    requires consulo.util.collection;
+    requires consulo.util.dataholder;
+    requires consulo.util.io;
+    requires consulo.util.lang;
+
     requires consulo.java;
     requires com.intellij.xml;
     requires com.intellij.xml.html.api;
